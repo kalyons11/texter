f2ae7f93137a28be2046b47c09e0afb7abc49a50
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-07-14 20:21:09 -0400

    Add print statement for datetime

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import send
 def get_git_revision_short_hash() -> str:
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
+print(datetime.datetime.now().isoformat())
 print(get_git_revision_short_hash())
 
 send.send(get_git_revision_short_hash())
