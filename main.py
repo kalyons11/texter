caaadeaee5b4a86267bfd288402435a6268c5beb
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-07-14 20:16:35 -0400

    Remove datetime as text message

--- main.py
+++ main.py
@@ -7,6 +7,4 @@ import send
 def get_git_revision_short_hash() -> str:
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
-current_time = datetime.datetime.now().isoformat()
-send.send(current_time)
 send.send(get_git_revision_short_hash())
