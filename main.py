3ee1c9c251a60df6db0b67c9b77837c49b7ab6a5
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-07-14 20:19:33 -0400

    Print things out

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ import send
 def get_git_revision_short_hash() -> str:
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
+print(get_git_revision_short_hash())
+
 send.send(get_git_revision_short_hash())
